bcc1704aa19c7e4f9ebe4ca147b0e84346eb149d
jbts6 <fh345392977@gmail.com>
2026-04-19 21:49:28 +0800

    feat(ui): add battle log panel skeleton

---
+++ addons/turn_manager/runtime/ui/battle_log_panel.gd
@@ -0,0 +1,10 @@
+extends Control
+class_name BattleLogPanel
+
+@onready var log_box: RichTextLabel = %LogBox
+
+
+func append_line(_bbcode_line: String) -> void:
+	# Skeleton; implementation will be added in next step.
+	pass
+
